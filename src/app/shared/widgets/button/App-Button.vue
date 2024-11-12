c07aa2138a63f9bb98892dc2d83e4e4da72c752f
kyle <kyle.chang@ackuretta.com>
2024-11-12 19:14:42 +0800

    style: extracted the css to a file for basic buttonj

---
+++ src/app/shared/widgets/button/App-Button.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import { onMounted, ref, computed } from 'vue';
+  import { defineProps, defineEmits } from 'vue';
+
+  const props = defineProps({
+    title: {
+      type: String,
+      required: true
+    },
+  });
+  const emits = defineEmits('buttonClick');
+  const count = ref(10);
+  const increment = () => {
+    count.value ++;
+    emits('buttonClick');
+  }
+  onMounted(() => {
+    console.log(`The initial count is ${count.value}.`)
+  })
+
+  const total = computed(() => count.value * 2);
+</script>
+
+<template>
+  Count {{ count }}<span> </span>
+  <button @click="increment" v-bind="$attrs">{{ props.title }}</button>
+{{ total }}
+</template>
+<style scoped lang="scss">
+  @import './app-button.scss';
+</style>
