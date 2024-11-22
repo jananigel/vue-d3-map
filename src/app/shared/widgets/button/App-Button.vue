f85d1355c2e76f440b939e51d2267cd43f76ac64
kyle <kyle.chang@ackuretta.com>
2024-11-22 18:53:06 +0800

    refactor: removed useless codes in the base button component

--- src/app/shared/widgets/button/App-Button.vue
+++ src/app/shared/widgets/button/App-Button.vue
@@ -1,6 +1,6 @@
 <script setup>
-  import { onMounted, ref, computed } from 'vue';
-  import { defineProps, defineEmits } from 'vue';
+  import { onMounted } from 'vue';
+  import { defineProps } from 'vue';
 
   const props = defineProps({
     title: {
@@ -8,23 +8,15 @@
       required: true
     },
   });
-  const emits = defineEmits('buttonClick');
-  const count = ref(10);
-  const increment = () => {
-    count.value ++;
-    emits('buttonClick');
-  }
+
   onMounted(() => {
-    console.log(`The initial count is ${count.value}.`)
+    // do nothing
   })
 
-  const total = computed(() => count.value * 2);
 </script>
 
 <template>
-  Count {{ count }}<span> </span>
-  <button @click="increment" v-bind="$attrs">{{ props.title }}</button>
-{{ total }}
+  <button v-bind="$attrs">{{ props.title }}</button>
 </template>
 <style scoped lang="scss">
   @import './app-button.scss';
